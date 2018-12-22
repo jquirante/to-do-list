f67ca4d69a694cd1caeb92bf38f240e9f5bd37b9
Justen Quirante <jcquirante@gmail.com>
2018-12-21 16:02:53 -0800

    Completed toggle functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,16 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize';
 import React, {Component} from 'react';
+import axios from 'axios';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
 import List from './list';
 import AddItem from './add_item';
 import dummyList from '../data/to_do_list';
 import { randomString } from '../helpers';
+
+const BASE_URL = 'http://api.reactprototypes.com/todos';
+const API_KEY = '?key=c1018_jquirante';
 
 class App extends Component {
     state = {
@@ -17,24 +21,59 @@ class App extends Component {
         this.getListData();
     }
 
-    addItem = (item) => {
-        const {list} = this.state;
-
-        item._id = randomString();
-
-        this.setState({
-            list: [{...item,_id: randomString()}, ...list]
-        })
+    addItem = async (item) => {
         
+        const response = await axios.post( BASE_URL + API_KEY, item);
+
+        console.log('Add Item Response: ', response);
+        this.getListData();
     }
 
-
-    getListData() {
+    
+    async getListData() {
         // Call server to get list data
 
-        this.setState({
-            list: dummyList,
-        });
+        // axios.get(BASE_URL + API_KEY).then( (response) => {
+        //     console.log('Get Todos Reponse: ',response);
+        // }).catch( (error)=> {
+        //     console.log('Error getting list data: ', error.message);
+        // });
+
+        try {
+            const response = await axios.get(BASE_URL + API_KEY);
+
+            console.log('Get Data Response: ', response);
+
+            this.setState({
+                list: response.data.todos
+            });
+        } catch (error) {
+            console.log('Something went wrong in getListData');
+        }
+
+
+    }
+
+    deleteItem = async (id) => {
+        console.log('Delete ID: ', id);
+        const response = await axios.delete(`${BASE_URL}/${id + API_KEY}`);
+        console.log('Delete response', response);
+        this.getListData();
+    }
+
+    toggleComplete = async(id) => {
+        console.log('Toggle ID: ',id);
+        try {
+            const response = await axios.put(`${BASE_URL}/${id + API_KEY}`);
+            console.log('Toggle Complete Response: ', response);
+            
+            this.getListData();
+        } catch (error) {
+            console.log('Toggle Catch Error: ', error);
+        }
+        
+        
+
     }
 
     render() {
@@ -45,7 +84,7 @@ class App extends Component {
             <div className="container">
                 <h1 className="center">To Do List</h1>
                 <AddItem add={this.addItem}/>
-                <List toDos={list}/>
+                <List toggle={this.toggleComplete} delete={this.deleteItem} toDos={list}/>
             </div>
 
         );
--- src/components/list.js
+++ src/components/list.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ListItem from './list_item';
 
 const List = (props) => {
     
     const listElements = props.toDos.map( (item) => {
-        return <li key={item._id} className="collection-item">{item.title}</li>
+        console.log('item', item)
+        return <ListItem toggle={() => props.toggle (item._id)} delete={() => props.delete(item._id)} complete={item.complete} key={item._id} title={item.title}/>
     });
 
     return (
