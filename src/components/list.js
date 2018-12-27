4450207995bb64ab7cd4096495175cf6248369d0
Justen Quirante <jcquirante@gmail.com>
2018-12-27 13:29:35 -0800

    Single Item view in progress

--- src/components/list.js
+++ src/components/list.js
@@ -6,7 +6,7 @@ const List = (props) => {
     
     const listElements = props.toDos.map( (item) => {
         console.log('item', item)
-        return <ListItem toggle={() => props.toggle (item._id)} delete={() => props.delete(item._id)} complete={item.complete} key={item._id} title={item.title}/>
+        return <ListItem itemId={item._id} toggle={() => props.toggle (item._id)} delete={() => props.delete(item._id)} complete={item.complete} key={item._id} title={item.title}/>
     });
 
     return (
